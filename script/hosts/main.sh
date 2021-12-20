c0f738c89cf199f0b24e863df2107e72e333ba1d
cumberjie <26396320+cumberjie@users.noreply.github.com>
2021-12-21 00:34:10 +0800

    Update main.sh

--- script/hosts/main.sh
+++ script/hosts/main.sh
@@ -8,7 +8,12 @@ curl -o i3.txt https://adaway.org/hosts.txt
 curl -o i4.txt https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardApps.txt
 curl -o i5.txt https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardMobileAds.txt
 curl -o i6.txt https://raw.githubusercontent.com/badmojr/1Hosts/master/mini/hosts.txt
-
+curl -o i7.txt https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts.txt
+curl -o i8.txt https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts
+curl -o i9.txt https://raw.githubusercontent.com/ilpl/ad-hosts/master/hosts
+curl -o i10.txt https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+curl -o i11.txt https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts
+curl -o i12.txt https://raw.githubusercontent.com/E7KMbb/AD-hosts/master/system/etc/hosts
 # Start Merge and Duplicate Removal
 cat i*.txt > mergd.txt
 cat mergd.txt | grep '^1'  > 1.txt
